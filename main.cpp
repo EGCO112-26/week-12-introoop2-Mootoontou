3c9b1653222f631edcb83ffe50021e5e34bee1cf
Pattavee Nimkulrat <144362140+Mootoontou@users.noreply.github.com>
2026-03-23 09:03:34 +0000

    Update student class and main to use argv for input, add display function, and show youngest students

--- main.cpp
+++ main.cpp
@@ -1,43 +1,37 @@
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <climits>
 using namespace std;
 
 #include "student.h"
-#define N 3
+
 int main(int argc, char* argv[]) {
-  student s1,s2;
+  int num_students = (argc - 1) / 2;
+  vector<student> students;
 
- s1.set_name("Amy"); s1.set_age(20); s2.set_name("James"); s2.set_age(17); s1.get_age()>s2.get_age() ? s1.print_name(); cout<< "is older":s2.print_name(); cout <<"is older";
-
-
-
-  student a[N];
-  // Set age and set name for the first two ppl
-  int i,age;
-  string n;
-   // 1. Ask for N names and N age using cin
- 
-  
- 
-  //2. Print name and age of all N ppl
-  
-  
-  
-  //3. Print name and age of 
-  //the youngest person
-  //Find what the age of the youngest person
- 
-  
-  // Print all info for the yougest person
- 
-  
-  
-  //4. Change input from cin to argv
-
-  
- return 0;
-
-    
+  for(int i = 0; i < num_students; i++){
+    students.push_back(student());
+    students[i].set_name(argv[1 + 2*i]);
+    students[i].set_age(atoi(argv[2 + 2*i]));
   }
+
+  // Find the minimum age
+  int min_age = INT_MAX;
+  for(auto& s : students){
+    if(s.get_age() < min_age) min_age = s.get_age();
+  }
+
+  // Print youngest students
+  cout << "Youngest student" << endl;
+  for(auto& s : students){
+    if(s.get_age() == min_age){
+      s.display();
+    }
+  }
+
+  return 0;
+}
   
   
 
--- student.h
+++ student.h
@@ -3,12 +3,16 @@ using namespace std;
 
 class student{
 private:
-
   string name;
+  int age;
 
 public:
-  
-   void set_name(string="Yme");
-    void print_name();
-
+  void set_name(string n = "Yme") { name = n; }
+  void set_age(int a) { age = a; }
+  int get_age() { return age; }
+  void print_name() { cout << name; }
+  void display() {
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+  }
 };
